5034946bc02d50d9548c3a4c6491644c9625925e
Gary Janes <gary_janes@hotmail.com>
2024-01-16 07:45:03 +0000

    Day 18 - use Hirst art image for colour palette

--- 018-turtle/main.py
+++ 018-turtle/main.py
@@ -1,5 +1,6 @@
 from random import randint
 from turtle import Turtle, Screen
+import colorgram
 
 
 def random_color():
@@ -9,6 +10,10 @@ def random_color():
     color = r, g, b
     return color
 
+
+# Extract colour pallet from Hirst art image
+# hirst_colors = colorgram.extract("th-1920986090", 20)
+hirst_colors = colorgram.extract("th-3637935002", 20)
 
 screen = Screen()
 screen.colormode(255)
@@ -23,6 +28,7 @@ turtle.speed(0)
 for j in range(-240, 270, 30):
     for i in range(-270, 300, 30):
         turtle.goto(i, j)
-        turtle.dot(15, random_color())
+        # turtle.dot(15, random_color())
+        turtle.dot(15, hirst_colors[randint(0, len(hirst_colors) - 1)].rgb)
 
 screen.exitonclick()
